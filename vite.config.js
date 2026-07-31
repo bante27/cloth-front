77359896e30a5b2311355ee118ff7ab9355744d2
bante27 <mitikubantalem@gmail.com>
2026-07-31 02:52:19 -0900

    updATE

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    host: true,       // ቪቴ በኮንቴይነር ውስጥ እንዲያዳምጥ ያደርጋል
+    host: true,      ደርጋል
     port: 3000,       // የቪቴ የውስጥ ፖርት
     strictPort: true,
     hmr: {
